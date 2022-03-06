5ec7aada5457ddc2e71e20fa45e358da86fe5345
Roland Bernard <rolli.bernard@gmail.com>
2022-03-06 18:52:40 +0100

    Implemented the apply macro

--- src/sequence.h
+++ src/sequence.h
@@ -2,12 +2,12 @@
 #define _SEQUENCE_H_
 
 #include "if-empty.h"
-#include "repeat.h"
+#include "apply.h"
 
 #define FIRST_HELPER(...) __VA_ARGS__ EMPTY (
 #define FIRST(...) FIRST_HELPER __VA_ARGS__ )
 #define REST(...) EMPTY __VA_ARGS__
 
-#define GET_SEQ_ELEMENT(I, SEQ) FIRST(EXPAND(REPEAT(I, EMPTY) SEQ))
+#define GET_SEQ_ELEMENT(I, SEQ) FIRST(APPLY(REST, I, SEQ))
 
 #endif
