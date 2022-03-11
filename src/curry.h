2a78843e50c975202d5140525bb82e0e357adc56
Roland Bernard <rolli.bernard@gmail.com>
2022-03-11 22:51:21 +0100

    Fixed bugs in the tagged union macros

--- src/curry.h
+++ src/curry.h
@@ -1,12 +1,15 @@
 #ifndef _CURRY_H_
 #define _CURRY_H_
 
-#include "if-empty.h"
-
 #define CURRY_CLOSE(...) __VA_OPT__(,) __VA_ARGS__ )
 #define CURRY_BEGIN(...) ( __VA_ARGS__
 
-#define CURRY_WITH(FUNC, ARGS) FUNC IF_NOT_EMPTY ARGS (CURRY_BEGIN ARGS CURRY_CLOSE)
+#define CURRY_TAKE(...) __VA_ARGS__
+#define CURRY_EMPTY(...)
+
+#define CURRY_IF_NOT_EMPTY(...) CURRY_EMPTY __VA_OPT__(() CURRY_TAKE)
+
+#define CURRY_WITH(FUNC, ARGS) FUNC CURRY_IF_NOT_EMPTY ARGS (CURRY_BEGIN ARGS CURRY_CLOSE)
 #define CURRY(FUNC, ...) CURRY_WITH(FUNC, (__VA_ARGS__))
 
 #endif
--- src/sequence.h
+++ src/sequence.h
@@ -1,13 +1,14 @@
 #ifndef _SEQUENCE_H_
 #define _SEQUENCE_H_
 
-#include "if-empty.h"
 #include "for-each.h"
 #include "apply.h"
 
-#define FIRST_HELPER(...) __VA_ARGS__ EMPTY (
+#define SEQ_EMPTY(...)
+
+#define FIRST_HELPER(...) __VA_ARGS__ SEQ_EMPTY (
 #define FIRST(...) FIRST_HELPER __VA_ARGS__ )
-#define REST(...) EMPTY __VA_ARGS__
+#define REST(...) SEQ_EMPTY __VA_ARGS__
 #define SECOND(...) FIRST(REST(__VA_ARGS__))
 
 #define GET_SEQ_ELEMENT(I, ...) FIRST(APPLY(REST, I, __VA_ARGS__))
